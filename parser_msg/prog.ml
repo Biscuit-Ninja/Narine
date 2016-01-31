3ed8cc995e1e8ba60c26db5ea668ecfbc3744bc4
Biscuit-Ninja <julien@grangier.fr>
2016-02-01 00:03:23 +0100

    commit all files

--- parser_msg/prog.ml
+++
@@ -1,8 +0,0 @@
-(* Fichier prog.ml *)
-open Msg (* Seulement si des fonctions d'affichage (pour les tests) sont présentes. *)
-
-(* Fonction principale (équivalent du main en C). *)
-let _ =
-  (* let lexbuf = Lexing.from_channel (open_in "Pascal.p") in *) (* Le code Pascal se trouve dans le fichier Pascal.p *)
-  let lexbuf = Lexing.from_string ":irc.iiens.net NOTICE AUTH :*** Looking up your hostname...\n\r" in
-let result = Parser.main Lexer.token lexbuf in () 
